7f2a64e0f913a263a66b288b646c9161b806964f
Lizzy Barron <ebarron@ualberta.ca>
2021-10-25 10:26:52 -0600

    main activity class for initial firestore functionality

--- code/app/src/main/java/com/example/habitup/MainActivity.java
+++ code/app/src/main/java/com/example/habitup/MainActivity.java
@@ -1,56 +1,182 @@
 package com.example.habitup;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Declare the variables so that you will be able to reference it later.
-    ListView cityList;
-    ArrayAdapter<City> cityAdapter;
-    ArrayList<City> cityDataList;
+    // Declaration of variables
+    FloatingActionButton searchBtn;
+    FloatingActionButton profileBtn;
+    ListView habitList;
+    ArrayAdapter<Habit> habitAdapter;
+    ArrayList<Habit> habitDataList;
 
-    CustomList customList;
+    Button addHabitButton;
+    EditText addNameText;
+    EditText addFrequencyText;
+    FirebaseFirestore db;
+
+    final String TAG = "TEST_LOG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cityList = findViewById(R.id.city_list);
+        // Initialize variables
+        habitList        = findViewById(R.id.habit_list);
+        addHabitButton   = findViewById(R.id.add_habit_btn);
+        addNameText      = findViewById(R.id.add_name_field);
+        addFrequencyText = findViewById(R.id.add_freq_field);
+        searchBtn        = findViewById(R.id.search_activity_btn);
 
-        String []cities ={"Edmonton", "Vancouver", "Toronto", "Hamilton", "Denver", "Los Angeles"};
-        String []provinces = {"AB", "BC", "ON", "ON", "CO", "CA"};
+        habitDataList = new ArrayList<>();
+        habitAdapter = new CustomList(this, habitDataList);
+        habitList.setAdapter(habitAdapter);
 
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference habitsRef     = db.collection("john23/habits/habitList");
 
-        cityDataList = new ArrayList<>();
+        // Add habit button listener
+        addHabitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String habitName   = addNameText.getText().toString();
+                final String habitFrequency = addFrequencyText.getText().toString();
+                /*  ^^ Could fail to parse, implement try catch exception clause later ^^  */
 
-        for(int i=0;i<cities.length;i++){
-            cityDataList.add((new City(cities[i], provinces[i])));
-        }
+                HashMap<String, String> data = new HashMap<>();
+                data.put("name", habitName);
+                data.put("frequency", habitFrequency);
 
-        cityAdapter = new CustomList(this, cityDataList);
+                habitsRef.document("habit" + String.valueOf(habitDataList.size()))
+                        .set(data)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d(TAG, "Document added.");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "Document not added: " + e.toString());
+                            }
+                        });
 
-        cityList.setAdapter(cityAdapter);
+                addNameText.setText("");
+                addFrequencyText.setText("");
+            }
+        });
 
-//        dataList = new ArrayList<>();
-//        dataList.addAll(Arrays.asList(cities));
-//
-//        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
-//
-//        cityList.setAdapter(cityAdapter);
+        // Collection event listener
+        habitsRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot documentSnapshots, @Nullable FirebaseFirestoreException error) {
+                habitDataList.clear();
 
+                for(QueryDocumentSnapshot doc : documentSnapshots) {
+                    String name = (String)doc.getData().get("name");
+                    String freq = (String)doc.getData().get("frequency");
+
+                    // Add each habit from database
+                    habitDataList.add(new Habit(name, freq));
+                }
+
+                habitAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // Delete habit (on click) listener
+        habitList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Delete habit?");
+                final String selectedName = habitDataList.get(pos).getName();
+                alert.setMessage("Do you want to delete '" + selectedName + "'?");
+                alert.setNegativeButton("Cancel", null);
+                alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Delete from our database
+                        habitsRef.document("habit" + String.valueOf(pos))
+                                .delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Log.d(TAG, "Document deleted.");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG, "Document failed to be deleted: " + e.toString());
+                                    }
+                                });
+
+                        // Notify adapter of change
+                        habitAdapter.notifyDataSetChanged();
+                    }
+                });
+
+                alert.show();
+            }
+        });
+
+        // Switch to SearchActivity
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchIntent = new Intent(view.getContext(), SearchActivity.class);
+                startActivity(switchIntent);
+            }
+        });
+
+        profileBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent profileSwitchIntent = new Intent(view.getContext(), Profile.class);
+                startActivity(profileSwitchIntent);
+            }
+
+        });
 
 
     }
-
 
 }

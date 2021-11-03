3e524132aa9724adaf9c05bc6bd687b268423a18
Lizzy Barron <ebarron@ualberta.ca>
2021-11-03 02:20:41 -0600

    adding in friends to the profile

--- code/app/src/main/java/com/example/habitup/ProfileActivity.java
+++ code/app/src/main/java/com/example/habitup/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.habitup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,11 +10,15 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ProfileActivity extends AppCompatActivity{
 
     ListView friendsList;
+    ArrayAdapter<String> friendsAdapter;
+    ArrayList<String> friendsDataList;
     Button backBtn;
+    Button viewRequestsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,31 @@ public class ProfileActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        friendsList = findViewById(R.id.friend_list);
-        ArrayList friendsListDataList = new ArrayList<>();
-        ArrayAdapter friendAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, friendsListDataList);
-        friendsList.setAdapter(friendAdapter);
+
 
         backBtn = findViewById(R.id.switch_activity_btn);
+        viewRequestsBtn = findViewById(R.id.view_requests_button);
+        friendsList = findViewById(R.id.friend_list);
+
+        String []friends = {"static_jacob39", "static_blake_o32", "static_katie_j"};
+
+        friendsDataList = new ArrayList<>();
+        friendsDataList.addAll(Arrays.asList(friends));
+
+        friendsAdapter = new ArrayAdapter<>(this, R.layout.profile_content, friendsDataList);
+
+        friendsList.setAdapter(friendsAdapter);
+
+
+        viewRequestsBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent requestsSwitchIntent = new Intent(v.getContext(), RequestsActivity.class);
+                startActivity(requestsSwitchIntent);
+            }
+        });
+
 
         // Return to main activity
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -35,5 +59,8 @@ public class ProfileActivity extends AppCompatActivity{
                 finish();
             }
         });
+
+
+
     }
 }

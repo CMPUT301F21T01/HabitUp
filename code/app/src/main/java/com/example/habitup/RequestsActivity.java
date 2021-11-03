93957fa25f0a87e558d6af84bfd6bf91357043c2
Lizzy Barron <ebarron@ualberta.ca>
2021-11-03 02:22:29 -0600

    setting up a way to view requests

---
+++ code/app/src/main/java/com/example/habitup/RequestsActivity.java
@@ -0,0 +1,39 @@
+package com.example.habitup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RequestsActivity extends AppCompatActivity {
+
+    ListView requestList;
+    ArrayAdapter<String> requestsAdapter;
+    ArrayList<String> requestsDataList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_requests);
+
+
+        requestList = findViewById(R.id.requests_list);
+
+        String[] requests = {"static_harrys", "static_niall_h72", "static_bey_kc"};
+
+        requestsDataList = new ArrayList<>();
+        requestsDataList.addAll(Arrays.asList(requests));
+
+        requestsAdapter = new ArrayAdapter<>(this, R.layout.requests_content, requestsDataList);
+
+        requestList.setAdapter(requestsAdapter);
+    }
+}

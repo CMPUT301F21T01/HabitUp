00490e1477300b8999d910ebfabe3db91bc974c2
unknown <kosmajac@ualberta.ca>
2021-11-25 14:00:45 -0700

    UserSyncer updated: add/edit/delete habit, handle/send friend requests.

--- code/app/src/main/java/com/example/habitup/UserSyncer.java
+++ code/app/src/main/java/com/example/habitup/UserSyncer.java
@@ -1,10 +1,10 @@
 package com.example.habitup;
 
-
-
+import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -13,6 +13,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * This class is responsible for keeping data consistent between local User (instance) and
@@ -32,6 +33,8 @@ public class UserSyncer {
     private CollectionReference friendsReference;
     private CollectionReference requestsReference;
     private CollectionReference habitsReference;
+
+    private final String TAG = "DEBUG_LOG";
 
     /**
      * Constructor set as private so other classes are unable to create instance(s).
@@ -61,13 +64,16 @@ public class UserSyncer {
      * @see FirebaseCallback
      */
     public void syncHabits(FirebaseCallback firebaseCallback) {
-        instance.user.clearHabits();
 
         instance.habitsReference.get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if(task.isSuccessful()) {
+                            // Clear habits
+                            instance.user.clearHabits();
+
+                            // Re-populate with new habits
                             for(QueryDocumentSnapshot document : task.getResult()) {
                                 // Extract data i.e. name, reason, etc.
                                 String name      = (String) document.getData().get("name");
@@ -136,7 +142,7 @@ public class UserSyncer {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if(task.isSuccessful()) {
                             for(QueryDocumentSnapshot document : task.getResult())
-                                instance.user.addFriend((String) document.getData().get("username"));
+                                instance.user.addFriend((String)document.getId());
                         }
                     }
                 });
@@ -148,12 +154,174 @@ public class UserSyncer {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if(task.isSuccessful()) {
                             for(QueryDocumentSnapshot document : task.getResult())
-                                instance.user.addRequest((String) document.getData().get("username"));
+                                instance.user.addRequest((String) document.getId());
                         }
                     }
                 });
 
         return instance.user;
+    }
+
+    /**
+     * Adds a habit to the user in Firestore. Also handles formatting the data appropriately
+     * so that it is stored properly in the database.
+     * @param habit to be added
+     */
+    public void addHabit(Habit habit) {
+
+        // Format data into a hash map
+        String frequencyString = String.join(",", habit.getFrequency());
+        HashMap<String, String> data = new HashMap<>();
+        data.put("name", habit.getTitle());
+        data.put("start date", habit.getStartDate());
+        data.put("end date", habit.getEndDate());
+        data.put("frequency", frequencyString);
+        data.put("reason", habit.getReason());
+        data.put("progress", habit.getProgress().toString());
+
+        // Add habit to User's habitList collection in Firestore
+        instance.habitsReference.document(habit.getTitle())
+                .set(data)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG,"Document not added: " + e.toString());
+                    }
+                });
+    }
+
+    /**
+     * Deletes a habit of the user from Firestore.
+     * @param habit to be deleted
+     */
+    public void deleteHabit(Habit habit) {
+        instance.habitsReference.document(habit.getTitle())
+                .delete()
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Document failed to be deleted: " + e.toString());
+                    }
+                });
+    }
+
+    /**
+     * Edits a habit of the user in Firestore.
+     * @param oldName previous name of the habit (prior to editing)
+     * @param habit to be edited / re-added
+     */
+    public void editHabit(String oldName, Habit habit) {
+
+        // Format data into a hash map
+        String frequencyString = String.join(",", habit.getFrequency());
+        HashMap<String, Object> updateData = new HashMap<>();
+        updateData.put("name", habit.getTitle());
+        updateData.put("start date", habit.getStartDate());
+        updateData.put("end date", habit.getEndDate());
+        updateData.put("frequency", frequencyString);
+        updateData.put("reason", habit.getReason());
+        updateData.put("progress", habit.getProgress().toString());
+
+        if(oldName == habit.getTitle()) {
+            // Update the document
+            instance.habitsReference.document(oldName)
+                    .update(updateData)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Document not added: " + e.toString());
+                        }
+                    });
+        } else {
+            // Otherwise the name was edited meaning we must delete and re-add the document
+
+            // Delete
+            instance.habitsReference.document(oldName)
+                    .delete()
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Document failed to be deleted: " + e.toString());
+                        }
+                    });
+
+            // Re-create
+            instance.habitsReference.document(habit.getTitle())
+                    .set(updateData)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Document not added: " + e.toString());
+                        }
+                    });
+        }
+
+    }
+
+    /**
+     * Accepts or rejects a friend request in Firestore.
+     * @param username to be accepted/rejected
+     * @param decision true if user is to be accepted or
+     *                 false otherwise
+     */
+    public void handleRequest(String username, boolean decision) {
+        // Delete from requests
+        instance.requestsReference.document(username)
+                .delete()
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Document failed to be deleted: " + e.toString());
+                    }
+                });
+
+        if(decision) {
+            // Add to current (friends)
+            HashMap<String, Object> filler = new HashMap<>();
+            instance.friendsReference.document(username)
+                    .set(filler)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Document failed to be created: " + e.toString());
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Sends a follower request to the username provided (in Firestore).
+     * @param username to request to follow
+     */
+    public void sendRequest(String username) {
+        CollectionReference foreignRequests = instance.db
+                .collection(username + "/friends/requests");
+
+        // Send request
+        HashMap<String, Object> filler = new HashMap<>();
+        foreignRequests.document(instance.user.getUsername())
+                .set(filler)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Document failed to be created: " + e.toString());
+                    }
+                });
+    }
+
+    /**
+     * Removes provided username from user's friend/follower list in Firestore.
+     * @param username to be removed
+     */
+    public void removeFriend(String username) {
+        instance.friendsReference.document(username)
+                .delete()
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Document failed to be deleted: " + e.toString());
+                    }
+                });
     }
 
     /**

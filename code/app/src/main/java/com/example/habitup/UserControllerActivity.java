faed43722469fda131bf2d610759cfd609cc8b56
unknown <kosmajac@ualberta.ca>
2021-11-28 14:35:24 -0700

    UserControllerActivity now properly handles signing in i.e. checks for existing user, checks that passwords match, displays error if any occur, etc.

--- code/app/src/main/java/com/example/habitup/UserControllerActivity.java
+++ code/app/src/main/java/com/example/habitup/UserControllerActivity.java
@@ -2,22 +2,27 @@ package com.example.habitup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * This activity provides an interface through which a user can sign in.
@@ -26,7 +31,10 @@ public class UserControllerActivity extends AppCompatActivity
 {
     // Variable declarations
     Button enterButton;
+    Button signUpButton;
     EditText usernameField;
+    EditText passwordField;
+
     FirebaseFirestore db;
     final String TAG = "DEBUG_LOG";
 
@@ -42,33 +50,68 @@ public class UserControllerActivity extends AppCompatActivity
         setContentView(R.layout.activity_user_controller);
 
         // Variable initializations
-        final String[] name = new String[1];
-        enterButton = findViewById(R.id.enter_button);
+        final String[] signInfo = new String[2];
+        enterButton   = findViewById(R.id.enter_button);
+        signUpButton  = findViewById(R.id.signup_button);
         usernameField = findViewById(R.id.username);
+        passwordField = findViewById(R.id.password);
 
         db = FirebaseFirestore.getInstance();
 
         enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                name[0] = usernameField.getText().toString();
-                CollectionReference userRef = db.collection(name[0]);
-                userRef.get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if(task.isSuccessful()) {
-                                    // Check if user exists
-                                    boolean userFound = false;
-                                    if(!task.getResult().isEmpty())
-                                        userFound = true;
+                signInfo[0] = usernameField.getText().toString();
+                signInfo[1] = passwordField.getText().toString();
+                if(signInfo[1].length() == 0) {
+                    Toast.makeText(getApplication().getBaseContext(),
+                            "Password must be provided.",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+                DocumentReference authRef = db.collection(signInfo[0]).document("auth");
+                authRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if(document.exists()) {
+                                if(signInfo[1].compareTo(document.getString("password")) == 0) {
+                                    // Pass username to HabitActivity intent and start activity
+                                    Intent intent = new Intent(view.getContext(), HabitActivity.class);
+                                    intent.putExtra(Intent.EXTRA_TEXT, signInfo[0]);
+                                    startActivity(intent);
+                                } else {
+                                    // Passwords do not match
+                                    passwordField.setBackgroundResource(R.drawable.sign_in_border_error);
+                                    Toast.makeText(getApplication().getBaseContext(),
+                                            "Incorrect password. Please try again.",
+                                            Toast.LENGTH_LONG).show();
+                                    passwordField.setText("");
+                                    Handler handler = new Handler();
+                                    handler.postDelayed(new Runnable() {
+                                        public void run() {
+                                            passwordField.setBackgroundResource(R.drawable.sign_in_border);
+                                        }
+                                    }, 3800);
+                                }
+                            }
+                            else {
+                                // Display that user does not exist
+                                usernameField.setBackgroundResource(R.drawable.sign_in_border_error);
+                                Toast.makeText(getApplication().getBaseContext(),
+                                        "Username does not exist.",
+                                        Toast.LENGTH_LONG).show();
 
-                                    if(userFound) {
-                                        ;
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    public void run() {
+                                        usernameField.setBackgroundResource(R.drawable.sign_in_border);
                                     }
-                                    else if (!userFound ) {
-                                        // Create auth document
-                                        HashMap<String, String> data = new HashMap<>();
+                                }, 3800);
+
+                                // Create auth document
+                                       /* HashMap<String, String> data = new HashMap<>();
                                         data.put("name", "Harry S.");
                                         data.put("password", "1b1ddd");
                                         userRef.document("auth")
@@ -78,18 +121,13 @@ public class UserControllerActivity extends AppCompatActivity
                                                     public void onFailure(@NonNull Exception e) {
                                                         Log.d(TAG, "Document not added: " + e.toString());
                                                     }
-                                                });
-                                    }
-
-                                    // Pass username to HabitActivity intent and start activity
-                                    Intent intent = new Intent(view.getContext(), HabitActivity.class);
-                                    intent.putExtra(Intent.EXTRA_TEXT, name[0]);
-                                    startActivity(intent);
-                                } else {
-                                    Log.d(TAG, "Error getting document: ", task.getException());
-                                }
+                                                });*/
                             }
-                        });
+                        } else {
+                            Log.d(TAG, "Error getting document: ", task.getException());
+                        }
+                    }
+                });
             }
         });
         // firestore authentication: https://firebase.google.com/docs/auth/android/start
